4cec20354a9eeec586fb2c35adb927a61e0d7828
iamtakagi <github@iamtakagi.net>
2024-02-21 00:18:18 +0900

    feat: configuration

--- src/main/java/com/github/iamtakagi/playerleveltag/PlayerLevelTag.java
+++ src/main/java/com/github/iamtakagi/playerleveltag/PlayerLevelTag.java
@@ -1,7 +1,10 @@
 package com.github.iamtakagi.playerleveltag;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,18 +13,69 @@ import com.nametagedit.plugin.NametagEdit;
 
 public class PlayerLevelTag extends JavaPlugin {
 
+  private Config config;
+
   @Override
   public void onEnable() {
-    this.getServer().getPluginManager().registerEvents(new Listener() {
-      @EventHandler
-      public void onJoin(PlayerJoinEvent event) {
-        NametagEdit.getApi().setPrefix(event.getPlayer(), "&7[&a" + event.getPlayer().getLevel() + "&7] &r");
+    if (!getServer().getPluginManager().isPluginEnabled("NametagEdit")) {
+      getLogger().severe("NametagEdit is not enabled or installed, Disabling...");
+      getServer().getPluginManager().disablePlugin(this);
+      return;
+    }
+    this.saveDefaultConfig();
+    this.config = new Config(this.getConfig());
+    this.getServer().getPluginManager().registerEvents(new ListenerImpl(), this);
+  }
+
+  class Config {
+    private Position position;
+    private String format;
+
+    public Config(FileConfiguration fileConfiguration) {
+      this.position = Position.valueOf(fileConfiguration.getString("tag.position", "PREFIX").toUpperCase());
+      this.format = fileConfiguration.getString("tag.format", "&7[&a%d&7] &r");
+    }
+
+    enum Position {
+      PREFIX, SUFFIX
+    }
+
+    public Position getPosition() {
+      return position;
+    }
+
+    public String getFormat() {
+      return format;
+    }
+  }
+
+  class ListenerImpl implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+      setLevelTag(event);
+    }
+
+    @EventHandler
+    public void onLevelChange(PlayerLevelChangeEvent event) {
+      setLevelTag(event);
+    }
+
+    private void setLevelTag(PlayerEvent event) {
+      Player player = event.getPlayer();
+      if (player == null)
+        return;
+      if (PlayerLevelTag.this.config.position == null || PlayerLevelTag.this.config.format == null)
+        return;
+
+      switch (PlayerLevelTag.this.config.position) {
+        case PREFIX:
+          NametagEdit.getApi().setPrefix(player, String.format(PlayerLevelTag.this.config.format, player.getLevel()));
+          break;
+        case SUFFIX:
+          NametagEdit.getApi().setSuffix(player, String.format(PlayerLevelTag.this.config.format, player.getLevel()));
+          break;
       }
-  
-      @EventHandler
-      public void onLevelChange(PlayerLevelChangeEvent event) {
-        NametagEdit.getApi().setPrefix(event.getPlayer(), "&7[&a" + event.getPlayer().getLevel() + "&7] &r");
-      }
-    }, this);
+    }
   }
 }
